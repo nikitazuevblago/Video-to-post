d962e45c7d2b1856aec4bea840a289f842d71d0d
nikitazuevblago <newzns1710@gmail.com>
2024-05-27 17:16:06 +0300

    starting creating VideoToPostBOT.py

--- VideoToPostBOT.py
+++ VideoToPostBOT.py
@@ -1,1 +1,54 @@
-BOT_TOKEN = "6933528952:AAEExBwDnbprx0xFvxlmIuC1DxDOwr3TQxo"
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from VideoToPost import VideoToPost
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "6933528952:AAEExBwDnbprx0xFvxlmIuC1DxDOwr3TQxo"
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, I'm a bot created by nikitazuevblago!")
+
+
+def check_new_video():
+    pass
+
+
+async def send_new_video():
+    while True:
+        video_url = check_new_video()
+        if video_url:
+            await bot.send_message(GROUP_CHAT_ID, f"New video uploaded: {video_url}")
+        await asyncio.sleep(60)  # Check every 60 seconds
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
